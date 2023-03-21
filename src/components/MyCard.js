afb2d0a62f1c362dd2779acd87ca36bc15044156
Dheeraj Vithalkar <dheerajrvithalkar@gmail.com>
2023-03-21 14:07:34 +0530

    Commiting first changes made in card

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,7 +1,26 @@
+import { Button, CardActions, CardContent, Typography } from '@mui/material';
 import React from 'react'
+import Card from '@mui/material/Card';
 
 const MyCard = () => {
-    return "MyCard"
+    const getMatchCard=()=>{
+        return (
+            <Card>
+                <CardContent>
+                    <Typography variant='h5'>This is our card</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant='contained' color='primary'>
+                        Show Details
+                    </Button>
+                    <Button variant='contained' color='primary'>
+                        {new Date().toString()}
+                    </Button>
+                </CardActions>
+            </Card>
+        )
+    };
+    return getMatchCard();
 }
 
 export default MyCard
